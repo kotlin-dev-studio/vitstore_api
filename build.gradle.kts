157e622f3988e86dc9daab87fd8709d96ba1a0ca
limkimhuor <kimhuorlim@gmail.com>
2021-01-26 23:05:42 +0700

    Mail catcher docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")
 
-
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(module = "mockito-core")
