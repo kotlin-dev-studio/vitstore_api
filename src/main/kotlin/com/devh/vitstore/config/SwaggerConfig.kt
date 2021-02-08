a8baae44e25c6c2727aae7b0d6fed21ae6e4e9eb
limkimhuor <kimhuorlim@gmail.com>
2021-02-08 14:56:08 +0700

    Task #629335 Update swagger OpenAPI 3.0

--- src/main/kotlin/com/devh/vitstore/config/SwaggerConfig.kt
+++ src/main/kotlin/com/devh/vitstore/config/SwaggerConfig.kt
@@ -2,26 +2,47 @@ package com.devh.vitstore.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiKey
+import springfox.documentation.oas.annotations.EnableOpenApi
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.HttpAuthenticationScheme
 import springfox.documentation.service.SecurityScheme
+import springfox.documentation.service.Tag
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 
 @Configuration
+@EnableOpenApi
 class SwaggerConfig {
 
     @Bean
-    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
-        .produces(HashSet(listOf("application/json")))
+    fun api(): Docket = Docket(DocumentationType.OAS_30)
+        .produces(HashSet(listOf(MediaType.APPLICATION_JSON_VALUE)))
         .select()
         .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
         .paths(PathSelectors.any())
         .build()
+        .useDefaultResponseMessages(false)
+        .apiInfo(apiInfo())
         .securitySchemes(listOf(apiKey()) as List<SecurityScheme>)
-    private fun apiKey(): ApiKey {
-        return ApiKey("Authorization", "Authorization", "header")
+        .tags(Tag("ID API", ""))
+
+    private fun apiKey(): HttpAuthenticationScheme {
+        return HttpAuthenticationScheme.JWT_BEARER_BUILDER
+            .name("Bearer")
+            .description("Enter JWT Bearer token **_only_**")
+            .build()
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title("API Documentation")
+            .description("API Description")
+            .version("1.0")
+            .build()
     }
 }
--- src/main/kotlin/com/devh/vitstore/controller/api/v1/sample/SampleController.kt
+++ src/main/kotlin/com/devh/vitstore/controller/api/v1/sample/SampleController.kt
@@ -1,11 +1,15 @@
 package com.devh.vitstore.controller.api.v1.sample
 
-import com.devh.vitstore.common.annotation.BearerHeaderToken
 import com.devh.vitstore.common.dto.ResultDataRes
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.Authorization
 import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.NoHandlerFoundException
 import java.io.Serializable
 import java.time.LocalDateTime
@@ -16,8 +20,11 @@ import javax.servlet.http.HttpServletRequest
 @RestController
 @RequestMapping("/api/v1/sample")
 class SampleController {
-    @GetMapping("/health")
-    @BearerHeaderToken
+    @GetMapping(
+        "/health",
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @ApiOperation(value = "Health check", authorizations = [Authorization(value = "Bearer")])
     fun health(): ResponseEntity<ResultDataRes<Health>> =
         ResponseEntity.ok(ResultDataRes.Success(Health("Alive at ${LocalDateTime.now()}")))
 
